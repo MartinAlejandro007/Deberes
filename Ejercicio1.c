58a4a88cdfb9002100dc521dce070ab0534e7801
Martin Estevez <martin.estevez@udla.edu.ec>
2026-03-29 17:28:44 -0500

    segundo commit. El programa solicita los valores

---
+++ Ejercicio1.c
@@ -0,0 +1,15 @@
+#include <stdio.h<
+
+int numero_numero_inicial;
+int numero_numero_final;
+int intervalo_de_incremento;
+int main() {
+    printf("Ingrese el numero inicial: ");
+    scanf("%d", &numero_numero_inicial);
+    printf("Ingrese el numero final: ");
+    scanf("%d", &numero_numero_final);
+    printf("Ingrese el intervalo de incremento: ");
+    scanf("%d", &intervalo_de_incremento);
+    }
+    return 0;
+}
